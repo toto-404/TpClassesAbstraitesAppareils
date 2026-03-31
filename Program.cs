3155c47412d23adc1ceec185922fc48a47a4b6f1
Olutobi <olutobi.akinsinmide@edouardgand.fr>
2026-03-31 11:19:42 +0200

    initialisation dans main avec collection appareil pour phone & ordi + réponse

--- Program.cs
+++ Program.cs
@@ -5,18 +5,31 @@ class Program
     static void Main(string[] args)
     {
         Ordinateur ordi1 = new Ordinateur("Lenovo", "Thinkpad", new DateTime(2020, 5, 15), 15.6);
-        ordi1.Allumer();
-        ordi1.AfficherType();
-        ordi1.AfficherGarantie();
-        ordi1.AfficherInfos();
-        Console.WriteLine(ordi1.ToString());
+        // ordi1.Allumer();
+        // ordi1.AfficherType();
+        // ordi1.AfficherGarantie();
+        // ordi1.AfficherInfos();
+        // Console.WriteLine(ordi1.ToString());
 
-        Console.WriteLine("---");
+        // Console.WriteLine("---");
 
         Smartphone phone1 = new Smartphone("Apple", "iPhone 12", new DateTime(2021, 10, 23), 3);
-        phone1.Allumer();
-        phone1.AfficherType();
-        phone1.AfficherGarantie();
-        phone1.AfficherInfos();
+        // phone1.Allumer();
+        // phone1.AfficherType();
+        // phone1.AfficherGarantie();
+        // phone1.AfficherInfos();
+
+        List <Appareil> appareils = new List<Appareil>();
+        appareils.Add(ordi1);
+        appareils.Add(phone1);
+
+        foreach (Appareil appareil in appareils)
+        {
+            Console.WriteLine("---");
+            appareil.Allumer();
+            appareil.AfficherType();
+            appareil.AfficherGarantie();
+            appareil.AfficherInfos();
+        }
     }
 }
